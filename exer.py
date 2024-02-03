3be3431d9324bfca5b46642cd6e1f966414b101b
mudimurtala <ibnmuhyideen95@gmail.com>
2024-02-03 11:41:16 +0100

    python

---
+++ exer.py
@@ -0,0 +1,11 @@
+# Prompt the user to enter a number
+user_input = input("Please enter a number: ")
+
+# Convert the input from a string to an integer
+number = int(user_input)
+
+# Double the number
+doubled_number = number * 2
+
+# Print the result
+print("The number doubled is:", doubled_number)
