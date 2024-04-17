4defa941288df2909c8e5b4a2084f279f4728771
mudimurtala <ibnmuhyideen95@gmail.com>
2024-04-17 16:42:59 +0100

    created a BaseModel

---
+++ MUDI_BnB/models/base_model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module defines a base class(parent class) for all models in my mbnb clone"""
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """A base class for all my mbnb model"""
+    def __int__(self, *args, **kwargs):
+        """Instantiate a new model"""
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
