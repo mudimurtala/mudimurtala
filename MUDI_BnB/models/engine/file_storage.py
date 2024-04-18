bae6e279509a58e73830ddc9395a1137f4fb102f
mudimurtala <ibnmuhyideen95@gmail.com>
2024-04-18 02:49:06 +0100

    Add a method called save()

--- MUDI_BnB/models/engine/file_storage.py
+++ MUDI_BnB/models/engine/file_storage.py
@@ -15,3 +15,13 @@ class FileStorage:
     def new(self, obj):
         """Add a new object to the storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+
+    def save(self):
+        """This method saves storage dictionary to file i.e. Serialization"""
+        with open(FileStorage.__file_path, 'w') as f:
+            temp = {}
+            temp.update(FileStorage.__objects)
+            for key, val in temp.items():
+                temp[key] = val.to_dict()
+            json.dump(temp, f)
+            
