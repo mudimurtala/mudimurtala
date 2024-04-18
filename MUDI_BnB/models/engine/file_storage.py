1bd5229d84e34c6f461121411b34115885529a1a
mudimurtala <ibnmuhyideen95@gmail.com>
2024-04-18 03:13:39 +0100

    Add a method called reload()

--- MUDI_BnB/models/engine/file_storage.py
+++ MUDI_BnB/models/engine/file_storage.py
@@ -3,6 +3,7 @@
 
 import json
 
+
 class FileStorage:
     """This class manages storage of my mbnb objects(models) in JSON format"""
     __file_path = 'file.json'
@@ -24,4 +25,28 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
-            
+
+
+    def reload(self):
+        """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
+        try:
+            temp = {}
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                        self.all()[key] = classes[val['__class__']](**val)
+        except FileNotFoundError:
+            pass
