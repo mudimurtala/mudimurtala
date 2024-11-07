f68d7bac9564b9255efe7a54df8bd3290f4273dc
mudimurtala <ibnmuhyideen95@gmail.com>
2024-11-07 12:22:58 +0100

    Add file

---
+++ react_fcc/my-app/src/MainContent.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+
+export default function MainContent() {
+    return (
+      <>
+        <h1>Reasons I'm excited to learn React</h1>
+        <ol>
+          <li>Because I want to be an expert Front-End Developer</li>
+          <li>Because React is used by most</li>
+          <li>Because it is easy to learn</li>
+          <li>Because of it's dominance in the industry</li>
+        </ol>
+      </>
+    )
+}
